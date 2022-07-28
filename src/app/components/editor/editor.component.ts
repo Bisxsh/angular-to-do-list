c596d2866053d459c4c0d2d085e5378a0fd41258
Bisesh <bisesh.sitaula@gmail.com>
2022-07-28 11:03:54 +0100

    Added and styled editor toolbar

--- src/app/components/editor/editor.component.ts
+++ src/app/components/editor/editor.component.ts
@@ -1,16 +1,26 @@
-import {Component, EventEmitter, Input, OnChanges, Output, SimpleChanges} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges
+} from '@angular/core';
 import {ITask} from "../../../interfaces/ITask";
 import * as Showdown from "showdown";
 import {TaskService} from "../../../services/task.service";
+import {EditorButtonMappings, LIST_BUTTONS, OTHER_BUTTONS, TEXT_BUTTONS} from "./util/Images";
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.css']
 })
-export class EditorComponent implements OnChanges {
+export class EditorComponent implements OnChanges, OnInit {
 
-  constructor(private service: TaskService) { }
+  constructor(private service: TaskService, private elementRef: ElementRef) { }
 
   @Input() activeTask!: ITask;
   @Input() write!: boolean;
@@ -18,6 +28,11 @@ export class EditorComponent implements OnChanges {
 
   taskContent: string = (this.activeTask) ? this.activeTask.description : "";
   contentTags!: string;
+  textArea!: any;
+
+  textButtons = TEXT_BUTTONS;
+  listButtons = LIST_BUTTONS;
+  otherButtons = OTHER_BUTTONS;
 
   converter = new Showdown.Converter({
     tables: true,
@@ -29,8 +44,8 @@ export class EditorComponent implements OnChanges {
   tasks!:ITask[];
   ngOnInit(): void {
     this.service.tasks.subscribe(t => this.tasks = t);
+    this.getDOMElement();
   }
-
 
   ngOnChanges(changes: SimpleChanges): void {
     for (let propName in changes) {
@@ -43,11 +58,30 @@ export class EditorComponent implements OnChanges {
     }
   }
 
+  getDOMElement() {
+    this.textArea = (<HTMLElement>this.elementRef.nativeElement).querySelector('.editor--write-input');
+  }
+
   onChange(event: any) {
     this.eventEmitter.emit(event.target.value);
     Promise.resolve(this.converter.makeHtml(event.target.value))
       .then(d => this.contentTags = d);
   }
 
+  toggleWrite() {
+    this.write = !this.write;
+  }
+
+  insertMarkdown(button: number) {
+    console.log(this.textArea);
+    let start = this.textArea.selectionStart;
+    // let end = this.textArea.selectionEnd;
+    let text = this.textArea.value;
+    switch (button) {
+      case EditorButtonMappings.HEADING:
+        this.textArea.value = text.substring(0, Math.max(start-4, 0)) + text.substring(Math.max(start-4, 0))
+        return;
+    }
+  }
 
 }
---
+++ src/app/components/editor/util/Images.ts
@@ -0,0 +1,84 @@
+
+export interface Images {
+  id: number,
+  iconName: string
+  alt: string
+}
+
+export enum EditorButtonMappings {
+  HEADING,
+  ITALIC,
+  STRIKETHROUGH,
+  BOLD,
+  BULLET_LIST,
+  NUMBER_LIST,
+  CHECK_LIST,
+  LINK,
+  QUOTES,
+  CODE,
+  PHOTO
+}
+
+export const TEXT_BUTTONS = [
+  {
+    id: 0,
+    iconName: "heading.png",
+    alt: "heading"
+  },
+  {
+    id: 1,
+    iconName: "italic.png",
+    alt: "italic"
+  },
+  {
+    id: 2,
+    iconName: "strikethrough.png",
+    alt: "strikethrough"
+  },
+  {
+    id: 3,
+    iconName: "bold.png",
+    alt: "bold"
+  }
+]
+
+export const LIST_BUTTONS = [
+  {
+    id: 4,
+    iconName: "bullet_list.png",
+    alt: "bullet_list"
+  },
+  {
+    id: 5,
+    iconName: "number_list.png",
+    alt: "number_list"
+  },
+  {
+    id: 6,
+    iconName: "check_list.png",
+    alt: "check_list"
+  }
+]
+
+export const OTHER_BUTTONS = [
+  {
+    id: 7,
+    iconName: "link.png",
+    alt: "link"
+  },
+  {
+    id: 8,
+    iconName: "quotes.png",
+    alt: "quotes"
+  },
+  {
+    id: 9,
+    iconName: "code.png",
+    alt: "code"
+  },
+  {
+    id: 10,
+    iconName: "photo.png",
+    alt: "photo"
+  }
+]
