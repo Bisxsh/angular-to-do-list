1dd211d851b2d8cd082eb549d633e66e279adae8
Bisesh <bisesh.sitaula@gmail.com>
2022-07-30 19:06:25 +0100

    Displayed selected category as main heading

--- src/app/components/tasks-list/tasks-list.component.ts
+++ src/app/components/tasks-list/tasks-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {ITask} from "../../../interfaces/ITask";
 import {TaskService} from "../../../services/task.service";
+import {Filters} from "../side-bar/util/Filters";
 
 @Component({
   selector: 'app-tasks-list',
@@ -13,10 +14,21 @@ export class TasksListComponent implements OnInit {
 
   tasks!:ITask[];
   activeTask!:ITask;
+  filterApplied!:number;
 
   ngOnInit(): void {
     this.service.tasks.subscribe(t => this.tasks = t);
     this.service.activeTask.subscribe(t => this.activeTask = t);
+    this.service.filterApplied.subscribe(f => this.filterApplied = f);
+  }
+
+  getFilterTitle() {
+    switch (this.filterApplied) {
+      default:
+      case (Filters.ALL_TASKS): return 'All Tasks';
+      case (Filters.WEEK): return 'This Week\'s Tasks';
+      case (Filters.TODAY): return 'Today\'s Tasks';
+    }
   }
 
 }
